f7321b588c8572db8bcf89fee2cd03f9c7d206ac
David Canales Parra <canalesparradavid@gmail.com>
2023-10-10 12:59:34 +0200

    Creada version 2, permite aumentar tiempo contador

--- src/countdown.c
+++ src/countdown.c
@@ -1,6 +1,6 @@
 /*
  * PROGRAMA: Countdown Counter
- * VERSION: 1.0
+ * VERSION: 1.1
  * HISTORICO: Creado el 09/10/2023 por David Canales Parra
  * DESCRIPCION: Este programa crea un temporizador que se puede inicializar con
  *              un tiempo específico pasado como argumento o por consola con el
@@ -8,11 +8,14 @@
  *              Una vez acabe el tiempo aparecera por pantalla y con color rojo
  *              un texto que diga "Tiempo Agotado" y se podrá cerrar el programa
  *              presionando enter.
+ *              Además permite aumentar el tiempo restante mediante el envio de
+ *              señales por teclado; en concreto CTRL-Z
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
 #include "clock.h"
 
@@ -30,24 +33,32 @@ int extractTime(char *formatedTime);
  */
 void getFormatedTime(Clock clock, char *time);
 
-int main(int argc, char **argv) {
-    Clock reloj;
-    int totalTime;
-    char actualTimeString[110];
+/*
+ * HANDLER: promptIncrementTime
+ * ENTRADA: Pide por teclado un aumento de tiempo
+ * MODIFICA: Crea un nuevo reloj con la suma del ambos tiempos
+ */
+void promptIncrementTime();
 
+// Variables GLOBALES
+char buff[1024];
+char actualTimeString[110];
+Clock reloj;
+
+int main(int argc, char **argv) {
     if (argc > 1) {
-        totalTime = extractTime(argv[1]);
+        strcpy(buff, argv[1]);
     }
     else {
-        char buff[1024];
         printf("Introduzca un tiempo con el formato HH:MM:ss -> ");
         scanf("%[^\n]s", buff);
-        totalTime = extractTime(buff);
     }
 
-    printf("\e[2J");
+    printf("\e[2J");    // Limpia la pantalla
 
-    newClock(&reloj, totalTime);
+    newClock(&reloj, extractTime(buff));
+
+    signal(SIGTSTP, promptIncrementTime);
 
     while (getTimeHours(reloj) != 0 || getTimeMinutes(reloj) != 0 || getTimeSeconds(reloj) != 0) {
         getFormatedTime(reloj, actualTimeString);
@@ -56,6 +67,8 @@ int main(int argc, char **argv) {
         sleep(1);
         decrement(&reloj);
     }
+
+    signal(SIGTSTP, NULL);
 
     char *textos[] = {"\x1b[31mFin del tiempo.\x1b[0m", "               "};
     while (1) {
@@ -95,4 +108,20 @@ int extractTime(char *formatedTime) {
 
 void getFormatedTime(Clock clock, char *time) {
     sprintf(time, "%02d:%02d:%02d", getTimeHours(clock), getTimeMinutes(clock), getTimeSeconds(clock));
+}
+
+void promptIncrementTime() {
+    int totalTime = getTotalTime(reloj);
+    getFormatedTime(reloj, actualTimeString);
+
+    printf("\e[2J\n");    // Limpia la pantalla
+    printf("Tiempo restante: %s\n", actualTimeString);
+    printf("Introduzca con el formato HH:MM:ss el tiempo que desea añadir: ");
+    scanf(" %[^\n]s", actualTimeString);
+
+    totalTime += extractTime(actualTimeString);
+
+    newClock(&reloj, totalTime);
+
+    printf("\e[2J");    // Limpia la pantalla
 }
